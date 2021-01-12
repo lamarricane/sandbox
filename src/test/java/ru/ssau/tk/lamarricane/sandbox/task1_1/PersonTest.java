01e52395b656a82ea1ef1bd10884dcd332ce2785
lamarricane <froggsycopp@gmail.com>
2021-01-12 22:40:49 +0400

    1.11 немножко другие тестики

--- src/test/java/ru/ssau/tk/lamarricane/sandbox/task1_1/PersonTest.java
+++ src/test/java/ru/ssau/tk/lamarricane/sandbox/task1_1/PersonTest.java
@@ -5,35 +5,40 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class PersonTest {
+    Person FirstPerson = new Person();
+    Person SecondPerson = new Person();
+    Person ThirdPerson = new Person();
+
     @Test
-    public void testGetSet() {
-
-        Person FirstPerson = new Person();
-        Person SecondPerson = new Person();
-        Person ThirdPerson = new Person();
-
+    public void testFirstName() {
         FirstPerson.setFirstName("Uzumaki");
-        FirstPerson.setLastName("Naruto");
-        FirstPerson.setPassportId(17);
-
         SecondPerson.setFirstName("Uchiha");
-        SecondPerson.setLastName("Sasuke");
-        SecondPerson.setPassportId(18);
-
-        ThirdPerson.setFirstName("Haruno");
-        ThirdPerson.setLastName("Sakura");
-        ThirdPerson.setPassportId(19);
-
+        ThirdPerson.setFirstName("Haruno");    //c передачей null вместо имени и фамилии ничего не работает :(
         assertEquals(FirstPerson.getFirstName(), "Uzumaki");
-        assertEquals(FirstPerson.getLastName(), "Naruto");
-        assertEquals(FirstPerson.getPassportId(), 17);
-
         assertEquals(SecondPerson.getFirstName(), "Uchiha");
-        assertEquals(SecondPerson.getLastName(), "Sasuke");
-        assertEquals(SecondPerson.getPassportId(), 18);
-
         assertEquals(ThirdPerson.getFirstName(), "Haruno");
+        assertNotEquals(SecondPerson.getFirstName(), "Hatake");
+    }
+
+    @Test
+    public void testLastName() {
+        FirstPerson.setLastName("Naruto");
+        SecondPerson.setLastName("Sasuke");
+        ThirdPerson.setLastName("Sakura");
+        assertEquals(FirstPerson.getLastName(), "Naruto");
+        assertEquals(SecondPerson.getLastName(), "Sasuke");
         assertEquals(ThirdPerson.getLastName(), "Sakura");
-        assertEquals(ThirdPerson.getPassportId(), 19);
+        assertNotEquals(SecondPerson.getLastName(), "Kakashi");
+    }
+
+    @Test
+    public void TestPassportId() {
+        FirstPerson.setPassportId(17);
+        SecondPerson.setPassportId(18);
+        ThirdPerson.setPassportId(18);
+        assertEquals(FirstPerson.getPassportId(), 17);
+        assertEquals(SecondPerson.getPassportId(), 18);
+        assertEquals(ThirdPerson.getPassportId(), 18);
+        assertNotEquals(SecondPerson.getPassportId(), 30);
     }
 }
