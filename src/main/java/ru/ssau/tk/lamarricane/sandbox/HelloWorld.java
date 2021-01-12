eed23574dad672bbcd3deb146a7f7e6642cf6b1b
lamarricane <froggsycopp@gmail.com>
2021-01-12 18:51:41 +0400

    Task_1.1 and Task_1.2

--- src/main/java/ru/ssau/tk/lamarricane/sandbox/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package ru.ssau.tk.lamarricane.sandbox;
-
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
---
+++ src/main/java/ru/ssau/tk/lamarricane/sandbox/task1_1/Person.java
@@ -0,0 +1,54 @@
+package ru.ssau.tk.lamarricane.sandbox.task1_1;
+
+import com.sun.org.apache.xerces.internal.xs.StringList;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int passportId;
+
+    Person () {
+        firstName="Имя";
+        lastName="Фамилия";
+        passportId=0;
+    }
+    Person (String firstName,String lastName,int passportId){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.passportId=passportId;
+    }
+    public Person (String firstName,String lastName) {
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+
+    Person (int passportId) {
+        this.passportId=passportId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPassportId() {
+
+        return passportId;
+    }
+
+    public void setPassportId(int passportId) {
+
+        this.passportId = passportId;
+    }
+}
